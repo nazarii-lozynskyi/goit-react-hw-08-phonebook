6b7e4f3d5ca1047283184e4c45d34df3ba24802a
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-24 12:51:22 +0100

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,5 +1,12 @@
 import { Button } from '@mui/material';
 
+import { Logout } from '@mui/icons-material';
+
 export default function UserMenu() {
-  return <Button color="inherit">Login</Button>;
+  return (
+    <Button color="inherit" sx={{ p: '8px', marginTop: '8px' }}>
+      Logout{' '}
+      <Logout sx={{ marginLeft: '5px', width: '32px', height: '32px' }} />
+    </Button>
+  );
 }
