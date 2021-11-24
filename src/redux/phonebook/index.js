3c9b902ff5e145ca761c16da2e8f9c127f224b99
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-24 02:01:47 +0100

    Create index.js

---
+++ src/redux/phonebook/index.js
@@ -0,0 +1,5 @@
+export * from './phonebook-actions';
+export * from './phonebook-reducer';
+export * from './phonebook-operations';
+export * from './phonebook-selectors';
+export * from './phonebook-types';
