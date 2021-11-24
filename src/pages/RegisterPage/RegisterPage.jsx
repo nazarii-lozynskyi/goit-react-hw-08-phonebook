2d51d1289afce27e8b9a0f47c7e36baf41552b42
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-24 04:29:05 +0100

    Update RegisterPage.jsx

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,5 +1,85 @@
+import * as React from 'react';
+
+import {
+  Box,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  FormControl,
+  IconButton,
+  TextField,
+  Button,
+} from '@mui/material';
+
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
+  return (
+    <Box>
+      <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined">
+        <TextField id="outlined-basic" label="Name" variant="outlined" />
+      </FormControl>
+
+      <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined">
+        <TextField
+          required
+          id="outlined-required"
+          label="email"
+          defaultValue=""
+        />
+      </FormControl>
+
+      <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={values.showPassword ? 'text' : 'password'}
+          value={values.password}
+          onChange={handleChange('password')}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+
+      <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined">
+        <Button variant="outlined">Registration</Button>
+      </FormControl>
+    </Box>
+  );
 }
 
 export default RegisterPage;
