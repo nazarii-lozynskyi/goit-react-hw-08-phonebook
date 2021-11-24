8e21aa33f912709da46985436b0939bde3a56245
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-24 04:27:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { AppBar, Container, Switch } from '@mui/material';
-import { Route } from 'react-router-dom';
+import { Container } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import ContactsPage from './pages/ContactsPage';
 import RegisterPage from './pages/RegisterPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+import MyAppBar from './components/MyAppBar';
 
 function App() {
   return (
     <Container>
-      <AppBar />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/contacts" components={ContactsPage} />
-      </Switch>
+      <MyAppBar />
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/registration" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </Container>
   );
 }
