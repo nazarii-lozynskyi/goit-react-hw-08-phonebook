3b8f06d89d7f45c353346595d3a97750d0eecc75
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-24 12:51:06 +0100

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,14 +1,64 @@
+import * as React from 'react';
+
 import { NavLink } from 'react-router-dom';
 
+import {
+  List,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon,
+  Box,
+} from '@mui/material';
+
+import { Login, HowToReg } from '@mui/icons-material';
+
 export default function AuthNav() {
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
   return (
-    <>
-      <NavLink to={'/login'} variant="h6" component="div">
-        Login
-      </NavLink>
-      <NavLink to={'/registration'} variant="h6" component="div">
-        Registration
-      </NavLink>
-    </>
+    <List component="nav" aria-label="navigation" sx={{ display: 'flex' }}>
+      <Box sx={{ width: '160px' }}>
+        <NavLink
+          to={'/login'}
+          variant="h6"
+          component="div"
+          style={{ textDecoration: 'none' }}
+        >
+          <ListItemButton
+            selected={selectedIndex === 0}
+            onClick={event => handleListItemClick(event, 0)}
+          >
+            <ListItemIcon>
+              <Login sx={{ width: '32px', height: '32px', color: 'white' }} />
+            </ListItemIcon>
+            <ListItemText primary="Login" sx={{ color: 'white' }} />
+          </ListItemButton>
+        </NavLink>
+      </Box>
+
+      <Box sx={{ width: '160px' }}>
+        <NavLink
+          to={'/registration'}
+          variant="h6"
+          component="div"
+          style={{ textDecoration: 'none' }}
+        >
+          <ListItemButton
+            selected={selectedIndex === 0}
+            onClick={event => handleListItemClick(event, 0)}
+          >
+            <ListItemIcon>
+              <HowToReg
+                sx={{ width: '32px', height: '32px', color: 'white' }}
+              />
+            </ListItemIcon>
+            <ListItemText primary="Registration" sx={{ color: 'white' }} />
+          </ListItemButton>
+        </NavLink>
+      </Box>
+    </List>
   );
 }
