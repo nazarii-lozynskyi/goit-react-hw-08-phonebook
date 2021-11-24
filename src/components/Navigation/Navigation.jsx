d86e55f61ff6459ab4958a8e83f64b78497016c2
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-24 12:51:20 +0100

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,66 @@
+import * as React from 'react';
+
 import { NavLink } from 'react-router-dom';
 
-export default function Navigation() {
+import {
+  List,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon,
+  Box,
+} from '@mui/material';
+
+import { Home, Notes } from '@mui/icons-material';
+
+export default function AuthNav() {
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
   return (
-    <>
-      <NavLink to={'/home'} variant="h6" component="div">
-        Home
-      </NavLink>
-      <NavLink to={'/notes'} variant="h6" component="div">
-        Notes
-      </NavLink>
-    </>
+    <List
+      component="nav"
+      aria-label="main mailbox folders"
+      sx={{ display: 'flex' }}
+    >
+      <Box sx={{ width: '160px' }}>
+        <NavLink
+          to={'/'}
+          variant="h6"
+          component="div"
+          style={{ textDecoration: 'none' }}
+        >
+          <ListItemButton
+            selected={selectedIndex === 0}
+            onClick={event => handleListItemClick(event, 0)}
+          >
+            <ListItemIcon>
+              <Home sx={{ width: '32px', height: '32px', color: 'white' }} />
+            </ListItemIcon>
+            <ListItemText primary="Home" sx={{ color: 'white' }} />
+          </ListItemButton>
+        </NavLink>
+      </Box>
+
+      <Box sx={{ width: '160px' }}>
+        <NavLink
+          to={'/notes'}
+          variant="h6"
+          component="div"
+          style={{ textDecoration: 'none' }}
+        >
+          <ListItemButton
+            selected={selectedIndex === 0}
+            onClick={event => handleListItemClick(event, 0)}
+          >
+            <ListItemIcon>
+              <Notes sx={{ width: '32px', height: '32px', color: 'white' }} />
+            </ListItemIcon>
+            <ListItemText primary="Notes" sx={{ color: 'white' }} />
+          </ListItemButton>
+        </NavLink>
+      </Box>
+    </List>
   );
 }
