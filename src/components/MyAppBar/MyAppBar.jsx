b6d266882e2ac8ab140a749685628814d4a9514d
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-24 04:51:54 +0100

    Update MyAppBar.jsx

--- src/components/MyAppBar/MyAppBar.jsx
+++ src/components/MyAppBar/MyAppBar.jsx
@@ -2,24 +2,31 @@ import Navigation from '../Navigation';
 import UserMenu from '../UserMenu';
 import AuthNav from '../AuthNav';
 
-import { Box, Toolbar, AppBar } from '@mui/material';
+import { ContactPhone } from '@mui/icons-material';
+import { Box, Toolbar, AppBar, Container, Icon } from '@mui/material';
 
 export default function MyAppBar() {
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-          <Box>
-            <Navigation />
-          </Box>
+          <Container style={{ maxWidth: '1650px' }}>
+            <Icon color="inherit" sx={{ width: '60px', height: '60px' }}>
+              <ContactPhone sx={{ width: '100%', height: '100%' }} />
+            </Icon>
 
-          <Box>
-            <AuthNav />
-          </Box>
+            <Box>
+              <Navigation />
+            </Box>
 
-          <Box>
-            <UserMenu />
-          </Box>
+            <Box>
+              <AuthNav />
+            </Box>
+
+            <Box>
+              <UserMenu />
+            </Box>
+          </Container>
         </Toolbar>
       </AppBar>
     </Box>
