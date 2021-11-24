5b1c441839f657c5c53fe84730c4409c4a16dc29
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-24 02:00:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
+import { AppBar, Container, Switch } from '@mui/material';
+import { Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import ContactsPage from './pages/ContactsPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
-  return <div></div>;
+  return (
+    <Container>
+      <AppBar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/contacts" components={ContactsPage} />
+      </Switch>
+    </Container>
+  );
 }
 
 export default App;
