cd3f205159eafcd251933537aec1d8d8d7d202e5
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-25 03:56:46 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,4 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import {
   persistStore,
   persistReducer,
@@ -8,57 +9,31 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { contactsReducer } from './phonebook';
+import { authReducer } from './auth';
 
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
 
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import logger from 'redux-logger';
-
-import contactsReducer from './phonebook/phonebook-reducer';
-import { phoneBookApi } from './phonebook/phonebook-slice';
-
-const contactsPersistConfig = {
-  key: 'Contacts',
+const authPersistConfig = {
+  key: 'auth',
   storage,
-  blacklist: ['filter'],
+  whitelist: ['token'],
 };
 
-const rootReducer = combineReducers({
-  contacts: persistReducer(contactsPersistConfig, contactsReducer),
-});
-
 export const store = configureStore({
-  reducer: { rootReducer, [phoneBookApi.reducerPath]: phoneBookApi.reducer },
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: contactsReducer,
+  },
+  middleware,
   devTools: process.env.NODE_ENV === 'development',
-  middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-    phoneBookApi.middleware,
-
-    logger,
-  ],
 });
 
-//export const store = configureStore({
-//  reducer: { rootReducer, [phoneBookApi.reducerPath]: phoneBookApi.reducer },
-//  devTools: process.env.NODE_ENV === 'development',
-//  middleware: getDefaultMiddleware =>
-//    getDefaultMiddleware({
-//      serializableCheck: {
-//        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//      },
-//    }).concat(logger),
-//});
-
-const persistor = persistStore(store);
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { store, persistor };
-
-//export default store;
-
-setupListeners(store.dispatch);
+export const persistor = persistStore(store);
