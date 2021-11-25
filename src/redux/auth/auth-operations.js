0ac0567162fa093f397013b780a3284101490baf
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-25 03:56:20 +0100

    Update auth-operations.js

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://lpj-tasker.herokuapp.com';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 const token = {
   set(token) {
@@ -12,9 +11,8 @@ const token = {
     axios.defaults.headers.common.Authorization = '';
   },
 };
-
 /*
- * POST @ /users/signup
+ * POST /users/signup
  * body: { name, email, password }
  * После успешной регистрации добавляем токен в HTTP-заголовок
  */
@@ -24,12 +22,11 @@ const register = createAsyncThunk('auth/register', async credentials => {
     token.set(data.token);
     return data;
   } catch (error) {
-    // TODO: Добавить обработку ошибки error.message
+    // Добавить обработку ошибки error.message
   }
 });
-
 /*
- * POST @ /users/login
+ * POST /users/login
  * body: { email, password }
  * После успешного логина добавляем токен в HTTP-заголовок
  */
@@ -39,12 +36,12 @@ const logIn = createAsyncThunk('auth/login', async credentials => {
     token.set(data.token);
     return data;
   } catch (error) {
-    // TODO: Добавить обработку ошибки error.message
+    // Добавить обработку ошибки error.message
   }
 });
 
 /*
- * POST @ /users/logout
+ * POST /users/logout
  * headers: Authorization: Bearer token
  * После успешного логаута, удаляем токен из HTTP-заголовка
  */
@@ -53,14 +50,12 @@ const logOut = createAsyncThunk('auth/logout', async () => {
     await axios.post('/users/logout');
     token.unset();
   } catch (error) {
-    // TODO: Добавить обработку ошибки error.message
+    // Добавить обработку ошибки error.message
   }
 });
 /*
- * GET @ /users/current
- * headers:
- *    Authorization: Bearer token
- *
+ * GET /users/current
+ 
  * 1. Забираем токен из стейта через getState()
  * 2. Если токена нет, выходим не выполняя никаких операций
  * 3. Если токен есть, добавляет его в HTTP-заголовок и выполянем операцию
@@ -72,7 +67,6 @@ const fetchCurrentUser = createAsyncThunk(
     const persistedToken = state.auth.token;
 
     if (persistedToken === null) {
-      console.log('Токена нет, уходим из fetchCurrentUser');
       return thunkAPI.rejectWithValue();
     }
 
@@ -81,7 +75,7 @@ const fetchCurrentUser = createAsyncThunk(
       const { data } = await axios.get('/users/current');
       return data;
     } catch (error) {
-      // TODO: Добавить обработку ошибки error.message
+      // Добавить обработку ошибки error.message
     }
   }
 );
